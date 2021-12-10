f2cb7a9858cf0a3b844aeed69f3105ceae4c8034
李佳骏 <73824503+jiajun-lab@users.noreply.github.com>
2021-12-10 22:37:24 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     # crawl(200, 1, 200)
 
     # 测试NLP，可将待处理文本的路径复制到filepath中，结果位于 src/NLP/jiebaVersion/result.txt
-    filepath = '/Users/lijiajun/Final-Proj-of-DataScience2021/~Archive/0.杨永保危险驾驶罪刑事一审刑事判决书.txt'
+    filepath = '/Users/lijiajun/Final-Proj-of-DataScience2021/~retrieved/1.雷某帮助信息网络犯罪活动罪、帮助信息网络犯罪活动罪刑事一审刑事判决书.txt'
     cal_word_frequency(filepath)
     get_result('/Users/lijiajun/Final-Proj-of-DataScience2021/src/NLP/jiebaVersion/wF.txt')
 
