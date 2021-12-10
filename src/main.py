e50f69c3368f8adc713e405ebdf7eab09cdbba98
李佳骏 <73824503+jiajun-lab@users.noreply.github.com>
2021-12-10 21:09:36 +0800

    文稿3.2 3.3

--- src/main.py
+++ src/main.py
@@ -5,13 +5,13 @@ from datetime import datetime
 from Crawling.crawling import crawl, clear
 from Crawling.text_extract import pkulaw_text_retrieve
 from Crawling import common
-from NLP.jiebaVersion.jiebaProcessing import calWordFrequency, getResult
+from NLP.jiebaVersion.jiebaProcessing import cal_word_frequency, get_result
 
 if __name__ == '__main__':
     # crawl(200, 1, 200)
 
     # 测试NLP，可将待处理文本的路径复制到filepath中，结果位于 src/NLP/jiebaVersion/result.txt
-    filepath = ''
-    calWordFrequency(filepath)
-    getResult('/Users/lijiajun/Final-Proj-of-DataScience2021/src/NLP/jiebaVersion/wF.txt')
+    filepath = '/Users/lijiajun/Final-Proj-of-DataScience2021/~Archive/0.杨永保危险驾驶罪刑事一审刑事判决书.txt'
+    cal_word_frequency(filepath)
+    get_result('/Users/lijiajun/Final-Proj-of-DataScience2021/src/NLP/jiebaVersion/wF.txt')
 
