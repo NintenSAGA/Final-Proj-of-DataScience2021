702b11ae3d9c86c53fcefcda5b01106e60f97ef0
李佳骏 <73824503+jiajun-lab@users.noreply.github.com>
2021-12-10 10:28:23 +0800

    っっっっb

--- src/main.py
+++ src/main.py
@@ -5,6 +5,10 @@ from datetime import datetime
 from Crawling.crawling import crawl, clear
 from Crawling.text_extract import pkulaw_text_retrieve
 from Crawling import common
+from NLP.jiebaVersion.jiebaProcessing import calWordFrequency, getResult
 
 if __name__ == '__main__':
-    crawl(200, 1, 200)
+    # crawl(200, 1, 200)
+    calWordFrequency('/Users/lijiajun/Final-Proj-of-DataScience2021/~retrieved/2.刘运宏危险驾驶罪刑事一审刑事判决书.txt')
+    getResult('/Users/lijiajun/Final-Proj-of-DataScience2021/src/NLP/jiebaVersion/wF.txt')
+
