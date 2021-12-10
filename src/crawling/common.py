04960ee3eca2d8c961555d125857393169a48baf
NintenSAGA <358546067@qq.com>
2021-12-10 21:44:50 +0800

    改进结构

--- src/crawling/common.py
+++ src/crawling/common.py
@@ -7,4 +7,4 @@ log = []
 html_folder = result_folder + '~html/'
 html_path = html_folder + '{}.html'
 refined_text_path = result_folder + '~refined_text/'
-noise_set = pickle.load(open(result_folder + '~noise_set.pkl', 'rb'))
+noise_set = pickle.load(open(result_folder + 'noise_set.pkl', 'rb'))
