d10f936860fab7f7c6b6b669800f7d30eda69a16
李佳骏 <73824503+jiajun-lab@users.noreply.github.com>
2021-12-15 22:16:39 +0800

    改变了get_result()的返回类型，基本完成

--- src/NLP/jiebaVersion/jiebaProcessing.py
+++ src/NLP/jiebaVersion/jiebaProcessing.py
@@ -3,7 +3,7 @@ import re
 import time
 import jieba.posseg as pseg
 import sys
-from NLP.get_filename import get_all
+from collections import OrderedDict
 
 # 全国法院名单.txt
 jieba.load_userdict('/Users/lijiajun/Final-Proj-of-DataScience2021/src/NLP/jiebaVersion/全国法院名单.txt')
@@ -15,35 +15,36 @@ jieba.load_userdict('/Users/lijiajun/Final-Proj-of-DataScience2021/src/NLP/jieba
 jieba.load_userdict('/Users/lijiajun/Final-Proj-of-DataScience2021/src/NLP/jiebaVersion/userdict.txt')
 
 
-def get_verdict(filepath):
-    """ 根据文本格式，获得判决结果
-
-        :parameter filepath:文档路径
-        :return Verdict
+def get_verdict(text):
     """
-    with open(filepath, 'r') as file:
-        lines = file.read().split('\n')
-        for i in range(len(lines)):
-            if ("判决如下" or "判决结果" or "判决主文") in lines[i]:
-                return lines[i+1]
+    根据文本格式，获得判决结果
+
+    :parameter text:文本 -> str
+    :return Verdict -> list
+    """
+
+    lines = text.split('\n')
+    for i in range(len(lines)):
+        if ("判决如下" or "判决结果" or "判决主文") in lines[i]:
+            return lines[i+1]
 
     return 'None'
 
 
-def get_danger_info(filepath):
+def get_danger_info(text):
     """
-        得到危险驾驶信息
+    得到危险驾驶信息
 
-        :parameter filepath:文本路径
-        :return danInfo
+    :parameter text:文本 -> str
+    :return danInfo -> str
     """
-    with open(filepath, 'r') as file:
-        lines = file.read().split('\n')
-        danInfo = 'None'
-        for i in range(len(lines)):
-            if re.search(r'\d+(\.\d+)?(ｍｇ|mg)([／/])\d+(ｍｌ|ml)', lines[i], re.DOTALL):
-                danInfo = re.search(r'\d+(\.\d+)?(ｍｇ|mg)([／/])\d+(ｍｌ|ml)', lines[i], re.DOTALL).group(0)
-                break
+
+    lines = text.split('\n')
+    danInfo = 'None'
+    for i in range(len(lines)):
+        if re.search(r'\d+(\.\d+)?(ｍｇ|mg)([／/])\d+(ｍｌ|ml)', lines[i], re.DOTALL):
+            danInfo = re.search(r'\d+(\.\d+)?(ｍｇ|mg)([／/])\d+(ｍｌ|ml)', lines[i], re.DOTALL).group(0)
+            break
 
     return danInfo
 
@@ -61,35 +62,35 @@ def process_property(filePath):
     file.close()
 
 
-def process_text(filepath):
-    """ 处理文本，过滤标点符号，并返回list
+def process_text(text):
+    """
+    处理文本，过滤标点符号，并返回list
 
-        :parameter filepath:文档路径
-        :return filterLines:过滤后的文本
+    :parameter text:文本 -> str
+    :return filterLines:过滤后的文本 -> list
     """
     filterLines = []
-    with open(filepath, 'r') as file:
-        for line in file:
-            line = re.sub(u"([^\u4e00-\u9fa5\u0030-\u0039\u0041-\u005a\u0061-\u007a])", "", line)
-            if line != '':
-                filterLines.append(list(pseg.cut(line, True)))
+    lines = text.split('\n')
+    for line in lines:
+        line = re.sub(u"([^\u4e00-\u9fa5\u0030-\u0039\u0041-\u005a\u0061-\u007a])", "", line)
+        if line != '':
+            filterLines.append(list(pseg.cut(line, True)))
 
-    file.close()
     return filterLines
 
 
-def cal_word_frequency(filepath):
+def cal_word_frequency(text):
     """
-        获得分词后每个词的词性以及词频，并按词性分类，按词频排序，并写入 wF.txt
+     获得分词后每个词的词性以及词频，并按词性分类，按词频排序，并写入 wF.txt
 
-        :parameter tex(list)：待切割的文本
+     :parameter tex(list)：待切割的文本
     """
-    text = process_text(filepath)
-    verdict = get_verdict(filepath)
-    danInfo = get_danger_info(filepath)
+    text1 = process_text(text)
+    verdict = get_verdict(text)
+    danInfo = get_danger_info(text)
     wordFrequency = {}
-    with open('/Users/lijiajun/Final-Proj-of-DataScience2021/src/NLP/jiebaVersion/wF.txt', 'w') as wFfile:
-        for line in text:
+    with open('./wF.txt', 'w') as wFfile:
+        for line in text1:
             for word in line:
                 if wordFrequency.get(str(word)):
                     wordFrequency[str(word)] += 1
@@ -104,34 +105,34 @@ def cal_word_frequency(filepath):
     wFfile.close()
 
 
-def write_result(wFfilepath, num):
+def return_result(wFfilepath):
     """
         计算得到结果
 
-        :param wFfilepath: 词频文本路径
-        :return: 得到文本分析结果
+        :parameter wFfilepath: 词频文本路径
+        :return result -> OrderedDict[str,list[str]]
     """
     # 个人基本信息
-    personInfo = ["姓名："]
+    personInfo = []
 
     # 地区
-    province = ["省份："]
-    city = ["城市："]
+    province = []
+    city = []
 
     # 罪名
-    case_cause = ["罪名："]
+    case_cause = []
 
     # 审理法院
-    court = ["法院："]
+    court = []
 
     # 判决结果
-    sentences = ["判决结果："]
+    sentences = []
 
     # 涉案金额
-    money = ["判处罚金："]
+    money = []
 
     # 危险驾驶相关信息
-    dan_message = ["酒精含量："]
+    dan_message = []
 
     with open(wFfilepath, 'r+') as wFfile:
         firstLine = wFfile.readline()
@@ -143,10 +144,10 @@ def write_result(wFfilepath, num):
             if words[len(words)-1] == 're':
                 for word in words:
                     if word != 're':
-                        sentences.append('\n\t' + word)
+                        sentences.append(word)
                     if re.search('(罚金|人民币).*元', word):
                         money.append(str(re.search('(罚金|人民币).*元', word).group(0)))
-                        sentences.append('\n\t' + word)
+                        sentences.append(word)
             elif words[1] == 'nr':
                 personInfo.append(words[0])
             elif words[1] == 'ct':
@@ -162,17 +163,29 @@ def write_result(wFfilepath, num):
                 elif len(words[0]) == 3:
                     case_cause.append(words[0])
 
+    result = OrderedDict()
+    result['姓名'] = personInfo
+    result['省份'] = province
+    result['城市'] = city
+    result['罪名'] = case_cause
+    result['审理法院'] = court
+    result['判决结果'] = sentences
+    result['罚金金额'] = money
+    result['危险驾驶相关信息'] = dan_message
 
-    new_file_name = '/Users/lijiajun/Final-Proj-of-DataScience2021/src/NLP/jiebaVersion/~result/'+str(num)+'标注.txt'
-    resultfile = open(new_file_name, 'w')
-    resultfile.write(to_string(personInfo) + '\n')
-    resultfile.write(to_string(province) + '\n')
-    resultfile.write(to_string(city) + '\n')
-    resultfile.write(to_string(case_cause) + '\n')
-    resultfile.write(to_string(court) + '\n')
-    resultfile.write(to_string(sentences) + '\n')
-    resultfile.write(to_string(money) + '\n')
-    resultfile.write(to_string(dan_message) + '\n')
+    # print(result)
+    return result
+
+    # new_file_name = '/Users/lijiajun/Final-Proj-of-DataScience2021/src/NLP/jiebaVersion/~result/'+str(num)+'标注.txt'
+    # resultfile = open(new_file_name, 'w')
+    # resultfile.write(to_string(personInfo) + '\n')
+    # resultfile.write(to_string(province) + '\n')
+    # resultfile.write(to_string(city) + '\n')
+    # resultfile.write(to_string(case_cause) + '\n')
+    # resultfile.write(to_string(court) + '\n')
+    # resultfile.write(to_string(sentences) + '\n')
+    # resultfile.write(to_string(money) + '\n')
+    # resultfile.write(to_string(dan_message) + '\n')
 
 
 def to_string(s):
@@ -190,21 +203,22 @@ def process_bar(num, total):
     sys.stdout.flush()
 
 
-def get_result(source_text_path, num):
+def get_result(text):
     """
-        param: source_text_path:文书所在文件夹路径
+        param: text -> 文书所在文件夹路径
+        return: result -> OrderedDict[str,list[str]]
     """
-    print("Log:获取文书路径")
-    time.sleep(1)
-    filepaths = get_all(source_text_path)
-    print("Log:已获取")
-    time.sleep(1)
-    i = 0
-    print("Log:开始处理")
-    for j in range(min(num, len(filepaths))):
-        cal_word_frequency(filepaths[j])
-        write_result('/Users/lijiajun/Final-Proj-of-DataScience2021/src/NLP/jiebaVersion/wF.txt', j)
-        i += 1
-        process_bar(i, num)
-        time.sleep(0.005)
+    # print("Log:获取文书路径")
+    # time.sleep(1)
+    # filepaths = get_all(source_text_path)
+    # print("Log:已获取")
+    # time.sleep(1)
+    # i = 0
+    # print("Log:开始处理")
+
+    cal_word_frequency(text)
+    return return_result('./wF.txt')
+    # i += 1
+    # process_bar(i, num)
+    # time.sleep(0.005)
 
--- src/main.py
+++ src/main.py
@@ -3,11 +3,12 @@ import time
 from datetime import datetime
 
 
-from NLP.jiebaVersion.jiebaProcessing import cal_word_frequency, get_result, process_property
+from NLP.jiebaVersion.jiebaProcessing import cal_word_frequency, get_result, return_result
 
 if __name__ == '__main__':
     # crawl(200, 1, 200)
-
+    with open("/Users/lijiajun/Final-Proj-of-DataScience2021/~Archive/2.刘运宏危险驾驶罪刑事一审刑事判决书.txt") as file:
+        text = file.read()
     # 测试NLP，可将待处理文本的路径复制到filepath中，结果位于 src/NLP/jiebaVersion/result.txt
-    get_result('/Users/lijiajun/Final-Proj-of-DataScience2021/~Archive', 10)
+    print(get_result(text))
     # process_property('/Users/lijiajun/Final-Proj-of-DataScience2021/src/NLP/jiebaVersion/罪名.txt')
