d3c489a99e44147bd4ee01174448cea61b8ae9b1
李佳骏 <73824503+jiajun-lab@users.noreply.github.com>
2022-01-13 11:50:54 +0800

    读了源码，源码，🐎

--- src/crawling/test_drive.py
+++ src/crawling/test_drive.py
@@ -3,7 +3,7 @@ from src.crawling.core.pkulaw import crawl_pkulaw
 from selenium.webdriver import Edge
 
 if __name__ == '__main__':
-    # crawl(100, year=2010, debug_mode=True)
+    #crawl(100, year=2010, debug_mode=True)
     with Edge() as edge:
         for year in range(2012, 2013):
             crawl_pkulaw(n=400, edge=edge, from_n=0, skip_fu=False, skip_rhf=False, year=year, debug_mode=True,
